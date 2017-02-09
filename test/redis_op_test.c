019d0244ae9320fcfb51d0f0e0bc6d92d0a65400
gedg33 <522520673@qq.com>
2017-02-09 09:53:00 +0800

    跟进老师进度

---
+++ test/redis_op_test.c
@@ -0,0 +1,75 @@
+<<<<<<< HEAD
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "redis_op.h"
+
+int main(int argc, char *argv[])
+{
+
+    //建立连接
+    redisContext* conn = NULL;
+
+    conn = rop_connectdb_nopwd("127.0.0.1", "6379");
+    if (conn == NULL) {
+        //...
+    }
+
+    //set key
+
+    //get key
+
+    //断开连接
+    rop_disconnect(conn);
+
+	return 0;
+}
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "redis_op.h"
+#include "make_log.h"
+
+#define TEST_MOUDLE  "test"
+#define REDIS_OP_PROC  "redis"
+
+int main(int argc, char *argv[])
+{
+    int retn = 0;
+    //建立连接
+    redisContext* conn = NULL;
+
+    conn = rop_connectdb_nopwd("127.0.0.1", "6379");
+    if (conn == NULL) {
+        //...
+        LOG(TEST_MOUDLE, REDIS_OP_PROC, "rop_connect error\n");
+        exit(1);
+    }
+
+    //set key
+    retn = rop_string_set(conn, "FOO", "BAR");
+    if (retn == 0) {
+        printf("set succ!\n");
+    }
+
+
+    //get key
+    char value[128] = {0};
+    retn = rop_string_get(conn, "FOO", value);
+    if (retn == 0) {
+        printf("get %s = %s\n", "FOO", value);
+    }
+
+    //断开连接
+    rop_disconnect(conn);
+
+	return 0;
+}
+>>>>>>> b549dcdc4a86ca9b114c3530e22f729a776d756a
